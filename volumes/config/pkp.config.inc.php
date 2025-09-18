6376cc9ded7cdbe4067b32219fa6ca8aac44fc95
John Vincent Gervacio <48063063+jvgervacio@users.noreply.github.com>
2025-09-18 11:25:39 +0800

    Add Traefik configuration and update routing script for OJS

---
+++ volumes/config/pkp.config.inc.php
@@ -0,0 +1,17 @@
+// In volumes/config/pkp.config.inc.php
+
+[general]
+
+; The base URL of the main site. This will be picked up by the script.
+base_url = "https://adminjournal.org"
+
+; Ensure restful_urls is On for clean URLs
+restful_urls = On
+
+...
+
+; --- Base URL overrides for specific journals ---
+; The script will also pick up all of these.
+base_url[hbgr] = "https://submissions.hgbr.org"
+base_url[gbej] = "https://submissions.gbej.com"
+base_url[path] = "https://submissions.path.ai"
