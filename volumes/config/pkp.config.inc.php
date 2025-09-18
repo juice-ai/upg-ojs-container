55a78ac86393cc66927a4c900c82c79a92bf879a
John Vincent Gervacio <48063063+jvgervacio@users.noreply.github.com>
2025-09-18 14:59:23 +0800

    Update base URLs to use HTTPS in configuration files and enhance Traefik network settings

--- volumes/config/pkp.config.inc.php
+++ volumes/config/pkp.config.inc.php
@@ -3,7 +3,7 @@
 [general]
 
 ; The base URL of the main site. This will be picked up by the script.
-base_url = "http://unijournal.org"
+base_url = "https://unijournal.org"
 
 ; Ensure restful_urls is On for clean URLs
 restful_urls = On
@@ -24,5 +24,5 @@ name = "env:MYSQL_DATABASE"
 
 ; --- Base URL overrides for specific journals ---
 ; The script will also pick up all of these.
-base_url[hbgr] = "http://submissions.hgbr.org"
-base_url[gbej] = "http://submissions.gbej.org"
+base_url[hbgr] = "https://submissions.hgbr.org"
+base_url[gbej] = "https://submissions.gbej.org"
