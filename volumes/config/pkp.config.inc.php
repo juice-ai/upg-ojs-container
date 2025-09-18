b5f120e436b5bfe1df3558fcb6e0527a3f268c8b
John Vincent Gervacio <48063063+jvgervacio@users.noreply.github.com>
2025-09-18 14:53:13 +0800

    Update base URL to use HTTP and configure database settings to use environment variables

--- volumes/config/pkp.config.inc.php
+++ volumes/config/pkp.config.inc.php
@@ -3,7 +3,7 @@
 [general]
 
 ; The base URL of the main site. This will be picked up by the script.
-base_url = "https://unijournal.org"
+base_url = "http://unijournal.org"
 
 ; Ensure restful_urls is On for clean URLs
 restful_urls = On
@@ -14,15 +14,15 @@ restful_urls = On
 ;;;;;;;;;;;;;;;;;;;;
 
 [database]
-driver = mysqli
-host = "db"
-username = "pkp"
-password = "changeMePlease" ; IMPORTANT: Use the value from your .env file's MYSQL_PASSWORD
-name = "pkp"
+driver = "env:DB_DRIVER"
+host = "env:DB_HOST"
+username = "env:MYSQL_USER"
+password = "env:MYSQL_PASSWORD"
+name = "env:MYSQL_DATABASE"
 
 ...
 
 ; --- Base URL overrides for specific journals ---
 ; The script will also pick up all of these.
-base_url[hbgr] = "https://submissions.hgbr.org"
-base_url[gbej] = "https://submissions.gbej.org"
+base_url[hbgr] = "http://submissions.hgbr.org"
+base_url[gbej] = "http://submissions.gbej.org"
